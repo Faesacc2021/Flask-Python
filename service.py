c3685bfcad76c75e37fc78d34d5f921f0184435c
Franz Blauth <faesacc2021@gmail.com>
2022-10-01 10:56:34 -0300

    xmlhttprequest test

--- service.py
+++ service.py
@@ -12,4 +12,4 @@ def service():
     return json.dumps(data)
   
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=False, threaded=True)
+    app.run(host='127.0.0.1', port=5000, debug=False, threaded=True)xA
