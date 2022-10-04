08f0228d1635d37c226d1f340338938dd02e5bc2
Franz Blauth <faesacc2021@gmail.com>
2022-10-04 17:12:02 -0300

    testes no serviço

--- service.py
+++ service.py
@@ -5,11 +5,17 @@ from flask import jsonify
 from requests import Response
 app = Flask(__name__)
 
-@app.route('/service', methods=['GET','POST'])
-def service():
+@app.route('/include', methods=['GET','POST'])
+def include():
     data = request.json
-    print(data)
+    print("Include")
+    return json.dumps(data)
+
+@app.route('/update', methods=['GET','POST'])
+def update():
+    data = request.json
+    print("Update")
     return json.dumps(data)
   
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=False, threaded=True)xA
+    app.run(host='127.0.0.1', port=5000, debug=False, threaded=True)
--- serviceUm.py
+++ serviceUm.py
@@ -1,11 +1,14 @@
 from json import dump
-import json
 from flask import Flask, request
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 from requests import Response
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 @app.route('/serviceUm', methods=['GET','POST'])
+@cross_origin(supports_credentials=True)
 def serviceUm():
     data = request.json
     print(data)
